64a49ddf819bd257a85be71ec336ab94fc1f9b31
Mark Eaton <MarkEEaton@users.noreply.github.com>
2024-03-30 19:01:02 -0400

    working fetch.py

--- fetch.py
+++ fetch.py
@@ -1,12 +1,31 @@
 import httpx
+import json
+import math
 import os
 from dotenv import load_dotenv
 from pprint import pprint
 
 load_dotenv()
 
-ELEC_COLLECTIONS = "https://api-na.hosted.exlibrisgroup.com/almaws/v1/electronic/e-collections?limit=100&offset=0&format=json&apikey="
-url = ELEC_COLLECTIONS + os.getenv("NZ_API_KEY")
+apikey = os.getenv("NZ_API_KEY")
+url = "https://api-na.hosted.exlibrisgroup.com/almaws/v1/electronic/e-collections?limit=100&offset={}&format=json&apikey={}"
 
-data = httpx.get(url, timeout=500)
-pprint(data)
+offset = 0
+data = httpx.get(url.format(offset, apikey), timeout=500)
+json_data = data.json()
+pages = math.ceil(json_data["total_record_count"] / 100)
+
+names = []
+
+for page in range(pages):
+    offset = page * 100
+    page_data = httpx.get(url.format(offset, apikey), timeout=500)
+    page_json = page_data.json()
+
+    for x in page_json["electronic_collection"]:
+        names.append(x["public_name"])
+
+sorted_names = sorted(names, key=str.casefold)
+
+with open('data.json', 'w') as f:
+    json.dump(sorted_names, f)
