bf7e6be34f6733b7ab590526814c277d668457f2
Mark Eaton <MarkEEaton@users.noreply.github.com>
2024-04-30 21:57:21 -0400

    show update time

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 import json
+import os
+import pytz
+from datetime import datetime
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+PATH = "/home/b7jl/electronic-collections-web-app/static/data.json"
 
-with open("/home/b7jl/electronic-collections-web-app/static/data.json", "r") as f:
+with open(PATH, "r") as f:
     data = json.load(f)
+
+ctime = os.path.getctime(PATH)
+utc_time = datetime.fromtimestamp(ctime).replace(tzinfo=pytz.utc)
+tz = pytz.timezone("America/New_York")
+dt = utc_time.astimezone(tz)
+output_time = dt.strftime("%-I:%M %p")
 
 
 @app.route("/")
 def index():
     count = len(data)
-    return render_template("index.html", count=count, data=data)
+    return render_template("index.html", count=count, data=data, time=output_time)
 
 
 @app.after_request
